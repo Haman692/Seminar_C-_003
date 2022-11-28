c6eb85221f53bc681aac281fe576ac2a5f56b40f
Haman692 <haman692@yandex.ru>
2022-11-29 01:38:46 +0700

    task017 and task018

---
+++ task_017/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.Clear();
+
+int DataEntryXY (string str)
+{
+    Console.Write(str);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void FindQvadnrant (int x, int y)
+{
+    if(x > 0 && y > 0) 
+        Console.WriteLine("Это первый квадрант");
+    if(x < 0 && y > 0) 
+        Console.WriteLine("Это второй квадрант");
+    if(x < 0 && y < 0) 
+        Console.WriteLine("Это третий квадрант");
+    if(x > 0 && y < 0) 
+        Console.WriteLine("Это четвертый квадрант");
+}
+
+
+
+int x =  DataEntryXY("Введите кординаты X = ");
+int y =  DataEntryXY("Введите кординаты Y = ");
+
+FindQvadnrant(x,y);
---
+++ task_018/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int DataQudrant(string str)
+{
+    Console.Write(str);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void PrintXY (int nuber)
+{
+    if(nuber < 1 || nuber > 4) Console.WriteLine("Не подходящее число");
+    else
+    {        
+        if(nuber == 1) Console.WriteLine("x > 0 и y > 0");
+        if(nuber == 2) Console.WriteLine("x < 0 и y > 0");
+        if(nuber == 3) Console.WriteLine("x < 0 и y < 0");
+        if(nuber == 4) Console.WriteLine("x > 0 и y < 0");
+    }
+}
+
+int Qudrant = DataQudrant("Напишите номер четверти ");
+
+PrintXY(Qudrant);
